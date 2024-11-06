39fd557e92c46ca63103aee694c57faac42a1cda
Nadeen Ahmed <74678959+nadeenahmed@users.noreply.github.com>
2024-11-06 19:15:48 +0200

    optimized query

--- Task1_Sol.sql
+++ Task1_Sol.sql
@@ -1,54 +1,30 @@
+-- Materialized View Creation
+
 CREATE MATERIALIZED VIEW player_profile AS
 WITH CurrentTeam AS(
 					SELECT 
 							p.playerID
-						   ,t.teamName
-						   ,p.name
+						   , t.teamName
+						   , p.name
 					FROM 
 						teams t 
 					JOIN 
 						players p ON t.teamID = p.teamID
 )
-, TotalGoals AS(
+, PlayerTotalStats AS(
 					SELECT 
 							p.playerID
 							, SUM(ps.Goals) TotalGoals
-					FROM 
-						player_stats ps 
-					JOIN 
-						players p ON p.playerID = ps.playerID
-					GROUP BY 
-						p.playerID
-					)
-, TotalAssists AS(
-					SELECT
-							p.playerID
 							, SUM(ps.Assists) TotalAssists
-					FROM 
-						player_stats ps 
-					JOIN 
-						players p ON p.playerID = ps.playerID
-					GROUP BY 
-						p.playerID
-					)
-, AverageMinutesPlayed AS(
-					SELECT
-							p.playerID
 							, AVG(ps.MinutesPlayed) AverageMinutesPlayed
-					FROM 
-						player_stats ps 
-					JOIN 
-						players p ON p.playerID = ps.playerID
-					GROUP BY 
-						p.playerID
-					)
-, PlayedOver300Min AS(
-					SELECT
-							p.playerID
 							, CASE
 								WHEN SUM(ps.MinutesPlayed) > 300 THEN B'1'
 								ELSE B'0'
-							END AS PlayedOver300Min				
+							END AS PlayedOver300Min		
+							, CASE
+								WHEN (EXTRACT(YEAR FROM AGE(NOW(), p.dateofbirth)) BETWEEN 25 AND 30) THEN B'1'
+								ELSE B'0'
+							END AS AgeBetween25And30		
 					FROM 
 						player_stats ps 
 					JOIN 
@@ -56,27 +32,15 @@ WITH CurrentTeam AS(
 					GROUP BY 
 						p.playerID
 					)
-, AgeBetween25And30 AS(
-					SELECT
-							p.playerID
-							, CASE
-								WHEN (EXTRACT(YEAR FROM AGE(NOW(), p.dateofbirth)) BETWEEN 25 AND 30) THEN B'1'
-								ELSE B'0'
-							END AS AgeBetween25And30				
-					FROM 
-						player_stats ps 
-					JOIN 	
-						players p ON p.playerID = ps.playerID
-					GROUP BY 
-						p.playerID
-					)
-, Scored3PlusGoalsInMatch AS(
+
+, PlayerMatchStats AS(
 					SELECT
 							p.playerID
 							, CASE
 								WHEN MAX(Goals) >= 3 THEN B'1'
 								ELSE B'0'
-							END AS Scored3PlusGoalsInMatch				
+							END AS Scored3PlusGoalsInMatch	
+							, CONCAT(SUM(minutesplayed) / 90, ' match', SUM(minutesplayed) % 90, ' min') AS EstimatedMatchesPlayed 
 					FROM 
 						player_stats ps 
 					JOIN 
@@ -86,26 +50,17 @@ WITH CurrentTeam AS(
 					GROUP BY 
 						p.playerID
 					)
-, EstimatedMatchesPlayed AS(
+, PlayedInFranceOrGermany AS(
 					SELECT
 							p.playerID
-							,CONCAT(SUM(minutesplayed) / 90, ' match', SUM(minutesplayed) % 90, ' min') AS EstimatedMatchesPlayed 				
-					FROM 
-						player_stats ps 
-					JOIN 
-						players p ON p.playerID = ps.playerID
-					JOIN 
-						matches m ON m.matchID = ps.matchID
-					GROUP BY 
-						p.playerID
-					)
-, PlayedInFrance AS(
-					SELECT
-							p.playerID
-							,CASE
+							, CASE
 								WHEN t.country = 'France' THEN B'1'
 								ELSE B'0' 
 							END AS PlayedInFrance
+							, CASE
+								WHEN t.country = 'Germany' THEN B'1'
+								ELSE B'0' 
+							END AS PlayedInGermany
 					FROM 
 						players p
 					JOIN 
@@ -114,7 +69,7 @@ WITH CurrentTeam AS(
 , DateJoinedFrenchTeam AS(
 					SELECT
 							p.playerID
-							,MIN(th.transferdate) AS DateJoinedFrenchTeam
+							, MIN(th.transferdate) AS DateJoinedFrenchTeam
 					FROM 
 						players p
 					JOIN 
@@ -126,22 +81,10 @@ WITH CurrentTeam AS(
 					GROUP BY 
 						p.playerID
 					)
-, PlayedInGermany AS(
-					SELECT
-							p.playerID
-							,CASE
-								WHEN t.country = 'Germany' THEN B'1'
-								ELSE B'0' 
-							END AS PlayedInGermany
-					FROM 
-						players p
-					JOIN 	
-						teams t ON t.teamID = p.teamID
-					)
 , DateJoinedGermanTeam AS(
 					SELECT
 							p.playerID
-							,MIN(th.transferdate) AS DateJoinedGermanTeam
+							, MIN(th.transferdate) AS DateJoinedGermanTeam
 					FROM 
 						players p
 					JOIN 
@@ -157,41 +100,39 @@ SELECT
 	CurrentTeam.PlayerID
 	, CurrentTeam.Name
 	, CurrentTeam.TeamName
-	, TotalGoals.TotalGoals
-	, TotalAssists.TotalAssists
-	, AverageMinutesPlayed.AverageMinutesPlayed
-	, PlayedOver300Min.PlayedOver300Min
-	, AgeBetween25And30.AgeBetween25And30
-	, Scored3PlusGoalsInMatch.Scored3PlusGoalsInMatch
-	, EstimatedMatchesPlayed.EstimatedMatchesPlayed
-	, PlayedInFrance.PlayedInFrance
+	, PlayerTotalStats.TotalGoals
+	, PlayerTotalStats.TotalAssists
+	, PlayerTotalStats.AverageMinutesPlayed
+	, PlayerTotalStats.PlayedOver300Min
+	, PlayerTotalStats.AgeBetween25And30
+	, PlayerMatchStats.Scored3PlusGoalsInMatch
+	, PlayerMatchStats.EstimatedMatchesPlayed
+	, PlayedInFranceOrGermany.PlayedInFrance
 	, DateJoinedFrenchTeam.DateJoinedFrenchTeam
-	, PlayedInGermany.PlayedInGermany
+	, PlayedInFranceOrGermany.PlayedInGermany
 	, DateJoinedGermanTeam.DateJoinedGermanTeam
 FROM 
 	CurrentTeam
 	JOIN 
-		TotalGoals ON CurrentTeam.playerid = TotalGoals.playerid
+		PlayerTotalStats on CurrentTeam.playerID = PlayerTotalStats.playerID
 	JOIN 
-		TotalAssists ON CurrentTeam.playerid = TotalAssists.playerid
+		PlayerMatchStats ON CurrentTeam.playerID = PlayerMatchStats.playerID
 	JOIN 
-		AverageMinutesPlayed ON CurrentTeam.playerid = AverageMinutesPlayed.playerid
-	JOIN 
-		PlayedOver300Min ON CurrentTeam.playerid = PlayedOver300Min.playerid
-	JOIN 
-		AgeBetween25And30 ON CurrentTeam.playerid = AgeBetween25And30.playerid
-	JOIN 
-		Scored3PlusGoalsInMatch ON CurrentTeam.playerid = Scored3PlusGoalsInMatch.playerid
-	JOIN 
-		EstimatedMatchesPlayed ON CurrentTeam.playerid = EstimatedMatchesPlayed.playerid
-	JOIN 
-		PlayedInFrance ON CurrentTeam.playerid = PlayedInFrance.playerid
+		PlayedInFranceOrGermany ON CurrentTeam.playerID = PlayedInFranceOrGermany.playerID
 	LEFT JOIN 	
-		DateJoinedFrenchTeam ON CurrentTeam.playerid = DateJoinedFrenchTeam.playerid
-	JOIN 
-		PlayedInGermany ON CurrentTeam.playerid = PlayedInGermany.playerid
+		DateJoinedFrenchTeam ON CurrentTeam.playerID = DateJoinedFrenchTeam.playerID
 	LEFT JOIN 
-		DateJoinedGermanTeam ON CurrentTeam.playerid = DateJoinedGermanTeam.playerid
-	
+		DateJoinedGermanTeam ON CurrentTeam.playerID = DateJoinedGermanTeam.playerID
+
+		
+		
+-- Display View Data
+
 SELECT *
 FROM football_league.player_profile;
+
+
+-- Refresh The View After Updates
+
+REFRESH MATERIALIZED VIEW football_league.player_profile;
+
