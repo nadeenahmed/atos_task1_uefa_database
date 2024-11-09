d878a5b1b57f0d8b5d4830ddca7be559d1c149a1
Nadeen Ahmed <74678959+nadeenahmed@users.noreply.github.com>
2024-11-09 13:40:14 +0200

    trigger function added and output file

--- Task1_Sol.sql
+++ Task1_Sol.sql
@@ -1,3 +1,8 @@
+UPDATE teams
+SET country = 'Germany'
+WHERE country = 'Italy'
+
+
 -- Materialized View Creation
 
 CREATE MATERIALIZED VIEW player_profile AS
@@ -50,52 +55,20 @@ WITH CurrentTeam AS(
 					GROUP BY 
 						p.playerID
 					)
-, PlayedInFranceOrGermany AS(
-					SELECT
-							p.playerID
-							, CASE
-								WHEN t.country = 'France' THEN B'1'
-								ELSE B'0' 
-							END AS PlayedInFrance
-							, CASE
-								WHEN t.country = 'Germany' THEN B'1'
-								ELSE B'0' 
-							END AS PlayedInGermany
-					FROM 
-						players p
-					JOIN 
-						teams t ON t.teamID = p.teamID
-					)
-, DateJoinedFrenchTeam AS(
-					SELECT
-							p.playerID
-							, MIN(th.transferdate) AS DateJoinedFrenchTeam
-					FROM 
-						players p
-					JOIN 
-						teams t ON t.teamID = p.teamID
-					JOIN 
-						transfer_history th ON th.toteamID = t.teamID AND p.playerID = th.playerID
-					WHERE 
-						t.country = 'France'
-					GROUP BY 
-						p.playerID
-					)
-, DateJoinedGermanTeam AS(
-					SELECT
-							p.playerID
-							, MIN(th.transferdate) AS DateJoinedGermanTeam
-					FROM 
-						players p
-					JOIN 
-						teams t ON t.teamID = p.teamID
-					JOIN 
-						transfer_history th ON th.toteamID = t.teamID AND p.playerID = th.playerID
-					WHERE 
-						t.country = 'Germany'
-					GROUP BY 
-						p.playerID
-					)
+, CountryPlayAndJoinDates AS (
+    SELECT
+        p.playerID,
+        MAX(CASE WHEN t.country = 'France' THEN 1 ELSE  0 END) AS PlayedInFrance,
+        MAX(CASE WHEN t.country = 'Germany' THEN 1 ELSE 0 END) AS PlayedInGermany,
+        MIN(CASE WHEN t.country = 'France' THEN th.transferdate END) AS DateJoinedFrenchTeam,
+        MIN(CASE WHEN t.country = 'Germany' THEN th.transferdate END) AS DateJoinedGermanTeam
+        
+    FROM players p
+    JOIN teams t ON t.teamID = p.teamID
+    LEFT JOIN transfer_history th ON th.toteamID = t.teamID AND p.playerID = th.playerID
+    GROUP BY p.playerID
+)
+
 SELECT 
 	CurrentTeam.PlayerID
 	, CurrentTeam.Name
@@ -107,22 +80,18 @@ SELECT
 	, PlayerTotalStats.AgeBetween25And30
 	, PlayerMatchStats.Scored3PlusGoalsInMatch
 	, PlayerMatchStats.EstimatedMatchesPlayed
-	, PlayedInFranceOrGermany.PlayedInFrance
-	, DateJoinedFrenchTeam.DateJoinedFrenchTeam
-	, PlayedInFranceOrGermany.PlayedInGermany
-	, DateJoinedGermanTeam.DateJoinedGermanTeam
+	, CountryPlayAndJoinDates.PlayedInFrance
+	, CountryPlayAndJoinDates.DateJoinedFrenchTeam
+	, CountryPlayAndJoinDates.PlayedInGermany
+	, CountryPlayAndJoinDates.DateJoinedGermanTeam
 FROM 
 	CurrentTeam
 	JOIN 
 		PlayerTotalStats on CurrentTeam.playerID = PlayerTotalStats.playerID
 	JOIN 
 		PlayerMatchStats ON CurrentTeam.playerID = PlayerMatchStats.playerID
-	JOIN 
-		PlayedInFranceOrGermany ON CurrentTeam.playerID = PlayedInFranceOrGermany.playerID
-	LEFT JOIN 	
-		DateJoinedFrenchTeam ON CurrentTeam.playerID = DateJoinedFrenchTeam.playerID
 	LEFT JOIN 
-		DateJoinedGermanTeam ON CurrentTeam.playerID = DateJoinedGermanTeam.playerID
+		CountryPlayAndJoinDates ON CurrentTeam.playerID = CountryPlayAndJoinDates.playerID
 
 		
 		
@@ -135,4 +104,54 @@ FROM football_league.player_profile;
 -- Refresh The View After Updates
 
 REFRESH MATERIALIZED VIEW football_league.player_profile;
+
+-- Create audit log table for manager_name
+CREATE TABLE manager_audit_log (
+    log_id SERIAL PRIMARY KEY,
+    team_id INT,
+    action_type VARCHAR(10),        
+    old_manager_name VARCHAR(255),
+    new_manager_name VARCHAR(255),
+    change_date TIMESTAMPTZ DEFAULT NOW()
+);
+
+-- Create a trigger before update or delete 
+CREATE OR REPLACE FUNCTION handle_manager_name_changes()
+   RETURNS TRIGGER
+   LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+ IF TG_OP = 'UPDATE' AND NEW.managerName IS DISTINCT FROM OLD.managername THEN
+       INSERT INTO manager_audit_log (team_id, action_type, old_manager_name, new_manager_name)
+       VALUES (OLD.teamID, 'UPDATE', OLD.managername, NEW.managername);
+ELSIF TG_OP = 'DELETE' THEN
+       INSERT INTO manager_audit_log (team_id, action_type, old_manager_name)
+       VALUES (OLD.teamID, 'DELETE', OLD.managername);
+END IF;   
+RETURN NEW;
+END;
+$$
+
+
+CREATE TRIGGER manager_name_trigger
+BEFORE UPDATE OF managerName OR DELETE
+ON teams
+FOR EACH ROW
+EXECUTE FUNCTION handle_manager_name_changes();
+
+-- Test the trigger function on update
+
+UPDATE teams
+SET managerName = 'José Romero'
+WHERE teamID = 1;
+
+-- Sample insert into teams table
+
+INSERT INTO teams (teamID, teamName, FoundedYear, HomeCity, managername, StadiumName, StadiumCapacity, Country)
+VALUES (21, 'Al-Ahly', 1907, 'Cairo', 'Marcel koller', 'Al Salam', 25000, 'Egypt');
+
+-- Test the trigger function on delete
+DELETE FROM teams
+WHERE teamID = 21;
 
